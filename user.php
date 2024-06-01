49b755b8a2ca54c779020c9d520c046d30d146ce
tjohnson2024 <troyadrian36@gmail.com>
2024-06-01 01:15:51 +0000

    created the user.php with a code to test if connected to database

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once ('database.php');
+
+Class User {
+
+public function get_all_users () {
+$db = db_connect();
+$statement = $db->prepare("select * from users;");
+  $statement->execute();
+$rows = $statement->fetch(PDO::FETCH_ASSOC);
+return $rows;
+}
