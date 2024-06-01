cb3188df8975bfd8236913a0f5e33807b9f6a57f
tjohnson2024 <troyadrian36@gmail.com>
2024-06-01 02:48:06 +0000

    just some changes to remowve a method not been used

--- user.php
+++ user.php
@@ -3,17 +3,6 @@
 require_once ('database.php');
 
 Class User {
-
-public function get_all_users () {
-$db = db_connect();
-$statement = $db->prepare("select * from users;");
-  $statement->execute();
-$rows = $statement->fetch(PDO::FETCH_ASSOC);
-return $rows;
-}
-
-  
-
   public function create_user($username, $password) {
       $db = db_connect();
       // Check if the username already exists
@@ -26,7 +15,8 @@ return $rows;
       $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
       // Insert new user into the database
-      $statement = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+      $statement = $db->prepare("INSERT INTO users (username, password) VALUES (:username, 
+      :password)");
       $statement->execute(array(':username' => $username, ':password' => $hashed_password));
       return "User created successfully";
   }
