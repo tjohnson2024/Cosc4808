47f282c4caef9ea33dadd4f00a4feea4a07bdc36
tjohnson2024 <troyadrian36@gmail.com>
2024-06-01 03:00:32 +0000

    made some adjustment to make sure password matches in the Registration process form

--- registration.php
+++ registration.php
@@ -10,8 +10,13 @@
     <form method="post" action="registration_process.php">
         <label for="new_username">New Username:</label><br>
         <input type="text" id="new_username" name="new_username"><br>
+
         <label for="new_password">New Password:</label><br>
-        <input type="password" id="new_password" name="new_password"><br><br>
+        <input type="password" id="new_password" name="new_password"><br>
+
+        <label for="retype_password">Retype Password:</label><br>
+        <input type="password" id="retype_password" name="retype_password"><br><br>
+
         <input type="submit" name="register" value="Register">
     </form>
 </body>
--- registration_process.php
+++ registration_process.php
@@ -4,13 +4,19 @@ session_start();
 // Include the User class
 require_once 'user.php';
 
-// Handle user registration
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register'])) {
     $new_username = isset($_POST['new_username']) ? $_POST['new_username'] : "";
     $new_password = isset($_POST['new_password']) ? $_POST['new_password'] : "";
+    $retype_password = isset($_POST['retype_password']) ? $_POST['retype_password'] : "";
 
-    $user = new User();
-    $registration_result = $user->create_user($new_username, $new_password);
-    echo $registration_result;
+    // Check if passwords match
+    if ($new_password !== $retype_password) {
+        echo "Passwords do not match. Please try again.";
+    } else {
+        // Proceed with user registration
+        $user = new User();
+        $registration_result = $user->create_user($new_username, $new_password);
+        echo $registration_result;
+    }
 }
 ?>
